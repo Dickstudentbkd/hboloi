e620a9a115335796bcc3e2c1a1894b614623aaa6
Dick BkD <bkd@hoornbeeck.nl>
2025-10-28 22:00:40 +0100

    Stand van zaken 28-10-2025

--- 02_javascript/opdrachten/33312SA1/js/game.js
+++ 02_javascript/opdrachten/33312SA1/js/game.js
@@ -2,6 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", resetHTML);
 class Board {
+    newCell;
     constructor(rows = 3, columns = 3) {
         this.rows = rows;
         this.columns = columns;
@@ -10,7 +11,7 @@ class Board {
          for (let i=0;i<rows;i++) {
             // const row = []; // voegt row toe
             for (let j=0;j<columns;j++) {
-                row.push(j); // voegt lege cel toe aan row
+                row.push(""); // voegt lege cel toe aan row
             }
             this.state.push(row); //voegt volledige row toe aan state...
         }
@@ -21,46 +22,95 @@ class Board {
         board.innerHTML = "";
         console.log('Rows: ' + this.rows + " Columns: " + this.columns);
         const startPlayer = this.startPlayer();
+        this.activePlayer = startPlayer;
         let player = document.createElement("div");
         player.setAttribute("id","player");
-        player.innerHTML = "Player: " + startPlayer;
+        player.innerHTML = "Player begin: " + startPlayer;
         board.appendChild(player);
+        //Active player aanwijzen
+        this.setActivePlayer(startPlayer);
 
         // create html-code...
         let iField = 0;
         for (let i = 0; i < this.rows; i++) {
             let newRow = document.createElement("div");
             newRow.setAttribute("class", "row");
+            newRow.setAttribute("data-value", i);
             for (let j = 0;j < this.columns;j++) {
                 iField++;
-                let newCell = document.createElement( "div");
-                newCell.setAttribute("class", "cell");
-                newCell.setAttribute("data-value", iField);
-                newRow.appendChild(newCell);
+                this.newCell = document.createElement( "div");
+                this.newCell.setAttribute("class", "cell");
+                this.newCell.setAttribute("data-value", j);
+                newRow.appendChild(this.newCell);
+                //op cell-niveau add event-listener
+                this.newCell.addEventListener("click", this.onClickField, true);
             }
             board.appendChild(newRow);
         }
-        board.addEventListener("click", this.onClickField);
+        // board.addEventListener("click", this.onClickField.bind(this), false);
     }
     //bij aanklikken van een veld
-    onClickField(e) {
-        let field = e.target.className;
-        let dataValue = e.target.getAttribute("data-value");
-        console.log('Field class: ' + field + "\n" + "Data-value: " + dataValue);
-        // alert("Yes, gelukt to zover!" + field + "\n" + "Data-value: " + dataValue);
-
-        e.target.className = field + " " + "x";
-        //Todo... tot zover werkt het; "x" moet actualPlayer worden. Deze moet bijgehouden worden!
+    onClickField = (e) => {
+        let field = e.target;
+        let rowDiv = e.target.closest(".row");
+        let rowValue = rowDiv.getAttribute("data-value");
+        let cellValue = e.target.getAttribute("data-value");
+        console.log('Field class: ' + field.className + " " + this.activePlayer.toLowerCase() + " Data-value: " + cellValue
+                        + "\n" + rowValue);
+        field.className = field.className + " " + this.activePlayer.toLowerCase();
+        //remove eventlistener...
+        field.removeEventListener("click", this.onClickField, true);
         //Daarna wegschrijven naar array
+        this.setValuePlayer(this.activePlayer, rowValue, cellValue);
+        this.isWinner(this.activePlayer);
+        //wissel player
+        this.changePlayer();
+        //instellen op board
+        this.setActivePlayer(this.activePlayer);
+
         //Daarna berekenen of er een win-situatie is
     }
     startPlayer() {
-        const players = ["X","Y"];
+        const players = ["X","O"];
         let randNumber = Math.floor((Math.random() * 2));
         return players[randNumber];
     }
+    //Beurt verwisselen player
+    changePlayer() {
+        this.activePlayer = this.activePlayer === "O" ? "X" : "O";
+    }
+    setActivePlayer(playerXO) {
+        const scoreBoardPlayer = document.getElementById("score");
 
-}
+        //scoreBoardPlayer.set VERDER (BkD 28-10-2025)
+        //active player: background default
+        //inactive player: background gray
+        if (playerXO === "X") {
+            scoreBoardPlayer.querySelector("div:nth-child(1)").style.removeProperty("background-color");
+                        // scoreBoardPlayer.querySelector("div:nth-last-child(1)").style.backgroundColor = "grey";
+            scoreBoardPlayer.querySelector("div:nth-of-type(2)").style.backgroundColor = "lightgray";
+            console.log("X: actief");
+        } else {
+            scoreBoardPlayer.querySelector("div:nth-child(1)").style.backgroundColor = "lightgray";
+            scoreBoardPlayer.querySelector("div:nth-of-type(2)").style.removeProperty("background-color");
+            console.log("O: actief");
+        }
+    }
+    setValuePlayer(player, iRow, iCell) {
+        // console.log("Player: " + player + " Row/Cell: " + iRow + "/" + iCell);
+        this.state[iRow][iCell] = player;
+        console.log("value (row/cell): " + iRow + "/" + iCell + " | " + this.state[iRow][iCell]);
+    }
+    isWinner(playerChar) {
+        //wincondition
+        let winc = inputWinCondition.value;
+        for (let r=0;r<this.state.length;r++) {
+            for (let c=0;c<this.state[r].length;c++) {
+                console.log(r + "/" + c + "|" + this.state[r][c])
+            }
+        }
+    }
+}//End class Board...
 
 const scores = document.getElementById("score");
 const scoreX = document.getElementById("score-x");
@@ -90,7 +140,7 @@ function resetHTML() {
     createBoard(3, 3); //default
 }
 function createBoard(rows, columns) {
-    const gameArea = new Board(rows, columns);
+    const gameArea = new Board(rows, columns); // aanmaken object vanuit class Board
     gameArea.createBoard("board");
     let playerStart = gameArea.startPlayer();
     console.log(playerStart);
@@ -112,11 +162,6 @@ function initializeSliders() {
     inputWinCondition.max = maxWinV;
     //opnieuw opbouwen gameArea/Board..
     createBoard(inputWidthV, inputHeightV);
-    // console.log("Width: " + inputWidthV);
-    // console.log("Height: " + inputHeightV);
-    // console.log("WIN: " + inputWinV);
 
-}
-function isWinner(playerChar) {
 
 }

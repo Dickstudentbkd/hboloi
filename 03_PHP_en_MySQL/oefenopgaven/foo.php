142b06d996c4952f2d524d8e77a3ff4ba28068f8
Dick BkD <bkd@hoornbeeck.nl>
2025-11-11 15:51:09 +0100

    Verzonden opdracht 33045FA2

---
+++ 03_PHP_en_MySQL/oefenopgaven/foo.php
@@ -0,0 +1,16 @@
+<?php
+$foo = "ipsum";
+
+function fun($bar) {
+    echo $bar;
+    //echo $foo;
+}
+fun('lorem');
+
+function prod($n1, $n2, $n3) {
+    $res = $n1 * $n2 * $n3;
+    echo "Het product is $res";
+}
+prod(1,2,3,4);
+is_obj();
+is_a();
